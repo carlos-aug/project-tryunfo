201751c5b50a3a857c26676d7d5e19c9bcd49306
Carlos Augusto <caarlos.san@gmail.com>
2022-07-27 17:08:55 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
       cardRare: '',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
+      cardSave: [],
     };
   }
 
@@ -42,30 +43,36 @@ class App extends React.Component {
 
     const validateString = cardName.length > 0
     && cardDescription.length > 0 && cardImage.length > 0 && cardRare.length > 0;
-    this.setState({
-      isSaveButtonDisabled: !validateString,
-    });
+    // this.setState({
+    //   isSaveButtonDisabled: !validateString,
+    // });
     // a soma de attr1, attr2 e attr3 não podem ultrapassar o valor 210
     const sumMaxAttrs = 210;
     const sumAttrs = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
-    const validateSum = sumAttrs > sumMaxAttrs;
-    if (validateString) {
-      this.setState({
-        isSaveButtonDisabled: !validateSum,
-      });
-      //  cada um dos attrs devem ter no minimo 0
-      //  maximo 90 pontos cada
-      // os attrs não podem receber valor negativo
-      const pointsAttrs = 90;
-      const maxPointsAttr1 = cardAttr1 >= 0 && cardAttr1 <= pointsAttrs;
-      const maxPointsAttr2 = cardAttr2 >= 0 && cardAttr2 <= pointsAttrs;
-      const maxPointsAttr3 = cardAttr3 >= 0 && cardAttr3 <= pointsAttrs;
+    const validateSum = sumAttrs <= sumMaxAttrs;
+    // if (validateString && validateSum) {
+    //   this.setState({
+    //     isSaveButtonDisabled: !validateSum,
+    //   });
+    //  cada um dos attrs devem ter no minimo 0
+    //  maximo 90 pontos cada
+    // os attrs não podem receber valor negativo
+    const pointsAttrs = 90;
+    const maxPointsAttr1 = cardAttr1 >= 0 && cardAttr1 <= pointsAttrs;
+    const maxPointsAttr2 = cardAttr2 >= 0 && cardAttr2 <= pointsAttrs;
+    const maxPointsAttr3 = cardAttr3 >= 0 && cardAttr3 <= pointsAttrs;
 
-      const verifyInputs = maxPointsAttr1 && maxPointsAttr2 && maxPointsAttr3;
-      this.setState({
-        isSaveButtonDisabled: !verifyInputs,
-      });
-    }
+    const verifyInputs = maxPointsAttr1 && maxPointsAttr2 && maxPointsAttr3;
+    const finalCheck = validateString && validateSum && verifyInputs;
+    this.setState({
+      isSaveButtonDisabled: !finalCheck,
+    });
+    // }
+  }
+
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+    console.log('ok');
   }
 
   render() {
@@ -81,7 +88,7 @@ class App extends React.Component {
       // hasTrunfo,
       isSaveButtonDisabled,
       // onInputChange,
-      onSaveButtonClick,
+      // onSaveButtonClick,
     } = this.state;
 
     return (
@@ -99,7 +106,7 @@ class App extends React.Component {
           // hasTrunfo,
           isSaveButtonDisabled={ isSaveButtonDisabled }
           onInputChange={ this.onInputChange }
-          onSaveButtonClick={ onSaveButtonClick }
+          onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           cardName={ cardName }
