a21c3ae1866896a54e053b3303eafcf3121e0fdc
Carlos Augusto <caarlos.san@gmail.com>
2022-07-27 11:18:26 -0300

    Requisito 5 - falta fazer condição para quando o numero é maior que 210

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
       cardAttr3: '',
       cardRare: '',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
 
@@ -23,7 +24,48 @@ class App extends React.Component {
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => this.enableButton());
+  }
+
+  // o botão deve iniciar desabilitado e só habilitar após todos os campos serem preenchidos
+  // os campos nome, descrição, imagem e raridade deve ter alguma informação
+  enableButton = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+    } = this.state;
+
+    const validateString = cardName.length > 0
+    && cardDescription.length > 0 && cardImage.length > 0 && cardRare.length > 0;
+    this.setState({
+      isSaveButtonDisabled: !validateString,
     });
+    // a soma de attr1, attr2 e attr3 não podem ultrapassar o valor 210
+    const sumMaxAttrs = 210;
+    const sumAttrs = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    const validateSum = sumAttrs > sumMaxAttrs;
+    if (validateString) {
+      this.setState({
+        isSaveButtonDisabled: !validateSum,
+      });
+      //  cada um dos attrs devem ter no minimo 0
+      //  maximo 90 pontos cada
+      // os attrs não podem receber valor negativo
+      const pointsAttrs = 90;
+      const maxPointsAttr1 = cardAttr1 >= 0 && cardAttr1 <= pointsAttrs;
+      const maxPointsAttr2 = cardAttr2 >= 0 && cardAttr2 <= pointsAttrs;
+      const maxPointsAttr3 = cardAttr3 >= 0 && cardAttr3 <= pointsAttrs;
+
+      const verifyInputs = maxPointsAttr1 && maxPointsAttr2 && maxPointsAttr3;
+      this.setState({
+        isSaveButtonDisabled: !verifyInputs,
+      });
+    }
   }
 
   render() {
